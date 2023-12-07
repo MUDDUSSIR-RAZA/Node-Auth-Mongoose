adc1a831f4403c178a1c525f3c430b91aa833b75
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-07 13:58:28 +0500

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,31 @@
 const { createUser, findUser } = require("../model/user");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (email, password) => {
+  console.log("user");
   try {
     const user = await findUser(email);
+    console.log(user);
     if (user) {
       return "User Already Exist!";
     }
-    const resp = await createUser(email, password);
+    const hashPass = await bcrypt.hash(password, 12);
+    const resp = await createUser(email, hashPass);
     return resp;
   } catch (err) {
     throw err;
   }
 };
 
-exports.login = (email, password) => {
+exports.login = async (email, password) => {
   try {
-    const user = findUser(email);
-    if (!!user) return "Wrong Email or Password";
-    return "User Successfully Login";
+    const user = await findUser(email);
+    if (!user) return "Wrong Email";
+    
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) return "Wrong Password";
+
+    return "Login Successfully";
   } catch (err) {
     throw err;
   }
