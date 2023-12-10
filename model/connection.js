6e52a4ae5ddcb9192f30fe2bb797a89dbf07a381
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-10 17:47:53 +0500

    Update connection.js

--- model/connection.js
+++ model/connection.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/auth";
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/auth")
-  .then(() => console.log("Connected!"));
+//mongoose.connect(url).then("connected");
+//module.exports = mongoose;
 
-module.exports = mongoose;
+exports.mongoose = async () => {
+  try {
+    let connect = await mongoose.connect(url);
+    console.log("connected");
+  } catch (err) {
+    throw err;
+  }
+};
