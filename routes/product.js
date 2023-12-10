eca7f001398bef7541560350092c15c078a3d2c4
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-10 19:18:20 +0500

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.send(req.email);
+  res.send(req.email );
 });
 
 module.exports = router;
